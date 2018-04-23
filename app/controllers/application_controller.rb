f114fa24aac447a219ae2af26d41e29a9b7de656
kitbuddy <me.ankit88@gmail.com>
2018-04-23 00:12:06 -0400

    this is got testing html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, How are You!!"
+    render html: "hello, How are You!! this is for testing!"
   end
 end
